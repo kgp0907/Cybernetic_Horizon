c45564d037c4f0b2d6fc4693867f7bff2d43eb16
rjrlarjsvy <rjrlarjsvy1124@gmail.com>
2022-05-29 21:24:28 +0900

    적 체력

---
+++ FSM/Robot/Enemy_HP.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Enemy_HP : MonoBehaviour
+{
+    Robot_Base robotp1;
+    public float hp_current=200;
+    public int hp;
+    public float EffectReturnTime=2f;
+    public bool dead;
+    public Image healthBarFilled;
+    public GameObject healthBarBackground;
+
+    private void Start()
+    {
+        hp_current = hp;
+        healthBarFilled.fillAmount = 1f;
+        robotp1 = Robot_Base.FindObjectOfType<Robot_Base>();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        hp_current -= damage;
+
+        if (hp_current <= 0&&!dead)
+        {
+            robotp1 = Robot_Base.FindObjectOfType<Robot_Base>();
+            dead = true;
+            robotp1.StopAllCoroutines();
+            robotp1.ChangeState(Robot_Base.RobotP1_State.DEAD);    
+        }
+
+        healthBarFilled.fillAmount = hp_current/hp;
+    }
+}
+
