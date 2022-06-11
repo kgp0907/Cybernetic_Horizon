766cd32cb5deed6fa462534882e032672c126e0c
rjrlarjsvy <rjrlarjsvy1124@gmail.com>
2022-06-11 16:36:03 +0900

    Move가 컨트롤러 역할을 하는것이 좋지 않다고 판단. 컨트롤러 스크립트를 통합하고 뉴 인풋시스템을 도입

--- FSM/Player/PlayerState/Player_State_Move.cs
+++ FSM/Player/PlayerState/Player_State_Move.cs
@@ -16,55 +16,12 @@ public class Player_State_Move : Interface_Base<Player>
 
     public void OnFixedUpdate(Player player)
     {
-        player.inputmanager.InputMovement();
-        player.transform.position += (player.inputmanager.velocity * Time.deltaTime);
-        player.inputmanager.Rotation();
+        player.playerCommand.Move();
+        player.playerCommand.Rotation();
     }
 
     public void OnUpdate(Player player)
     {
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            player.ChangeState(Player.playerState.NORMALATK1);
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            player.ChangeState(Player.playerState.DODGE);
-        }
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            player.ChangeState(Player.playerState.CHARGEATK);
-        }
-
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            if (player.useInventory)
-            {
-                player.OnApplicationFocus(player.useInventory);
-                player.useInventory = false;
-                player.inventoryUI.SetActive(false);
-            }
-
-            else
-            {
-                player.OnApplicationFocus(player.useInventory);
-                player.useInventory = true;
-                player.inventoryUI.SetActive(true);
-            }
-
-        }
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            SaveDataManager.Instance.SaveGameData();
-        }
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            LoadSceneManager.Instance.LoadScene("Main");
-        }
     }
-
 }
-
